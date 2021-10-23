a4568c0cc311b2106dc5101f98926aa2be3b50b7
The-Ripp3r <esr@mit.edu>
2021-10-23 17:10:39 -0400

    intergration of switching animal traits and integration handler

--- GodsPlayground/Assets/Scripts/Traits/TraitHandler.cs
+++ GodsPlayground/Assets/Scripts/Traits/TraitHandler.cs
@@ -5,40 +5,51 @@ using UnityEngine;
 public class TraitHandler : MonoBehaviour
 {
 
-    public List<Trait> levelOneTraits;
+    public List<Trait> bunnyTraits;
 
-    public List<Trait> levelTwoTraits;
+    public List<Trait> foxTraits;
 
     public List<Trait> allAvailableTraits = new List<Trait>();
 
     // Start is called before the first frame update
     void Start()
     {
-        // LEVEL ONE TRAITS
+        // LEVEL ONE TRAITS - BUNNIES?
         SpeedBoost1 sp1 = new GameObject().AddComponent<SpeedBoost1>();
 
         ShortenMateTime smt = new GameObject().AddComponent<ShortenMateTime>();
         
         ShortenGestationPeriod sgp = new GameObject().AddComponent<ShortenGestationPeriod>();
 
-        // LEVEL TWO TRAITS
+        // LEVEL TWO TRAITS - FOXES
         SpeedBoost2 sp2 = new GameObject().AddComponent<SpeedBoost2>();
 
-        levelOneTraits = new List<Trait>() { sp1, smt, sgp };
+        bunnyTraits = new List<Trait>() { sp1, smt, sgp };
 
-        levelTwoTraits = new List<Trait>() { sp2 };
+        foxTraits = new List<Trait>() { sp2 }; 
 
-        allAvailableTraits.AddRange(levelOneTraits);
-        allAvailableTraits.AddRange(levelTwoTraits);
+        allAvailableTraits.AddRange(bunnyTraits);
+        allAvailableTraits.AddRange(foxTraits);
     }
 
-    public List<Trait> GetRandomTraits(List<Trait> traits)
+    public List<Trait> GetRandomTraits(Species animal, int n)
     {
-        int numTraits = traits.Count;
-        if (numTraits < 3)
-        {
-            Debug.LogError("Not enough traits for UI!");
+        // int numTraits = traits.Count;
+        // if (numTraits < 3)
+        // {
+        //     Debug.LogError("Not enough traits for UI!");
+        // }
+        
+        List<Trait> roundTraits = new List<Trait>();
+        for (int i=0; i<n; i++){
+            if (animal==Species.Rabbit){
+                roundTraits.Add(bunnyTraits[Random.Range(0, bunnyTraits.Count)]);
+            }
+            else{
+                roundTraits.Add(foxTraits[Random.Range(0, foxTraits.Count)]);
+            }
         }
-        return new List<Trait>();
+
+        return roundTraits;
     }
 }

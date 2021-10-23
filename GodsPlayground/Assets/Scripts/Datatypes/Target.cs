b4224ad014852a46d3c80c5afaa8ec5e0600691b
Bryan Lopez <lopezb@mit.edu>
2021-10-23 17:10:18 -0400

    Saved Target Enum lol

--- GodsPlayground/Assets/Scripts/Datatypes/Target.cs
+++ GodsPlayground/Assets/Scripts/Datatypes/Target.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Target : MonoBehaviour
+public enum Target
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Undefined = 0,
+    Food = 1,
+    Water = 2,
+    Mate = 3,
+    RunAway = 4,
 }

01aae462bb83cabf8b48bf9da932fe8e81c35f87
Derek Velez <djvelez@mit.edu>
2021-11-01 18:19:44 -0400

    adding camera zoom locks

--- GodsPlayground/Assets/Scripts/UserControl.cs
+++ GodsPlayground/Assets/Scripts/UserControl.cs
@@ -26,18 +26,10 @@ public class UserControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalTranslation = Input.GetAxis("Horizontal") * (horizontalSpeed * Mathf.Clamp01((2 + transform.position.y) / 20.0f));
-        float verticalTranslation = Input.GetAxis("Vertical") * (verticalSpeed * Mathf.Clamp01((2 + transform.position.y) / 20.0f));
-        float zoomTranslation = Input.GetAxis("Zoom") * zoomSpeed;
         GameObject lineGraphCamera = lineGraphScript.gameObject.transform.GetChild(0).gameObject;
         GameObject populationChartCamera = populationChartScript.gameObject.transform.GetChild(0).gameObject;
 
-        horizontalTranslation *= Time.deltaTime;
-        verticalTranslation *= Time.deltaTime;
-        zoomTranslation *= Time.deltaTime;
-
-        transform.Translate(horizontalTranslation, 0, verticalTranslation, Space.World);
-        transform.Translate(0, 0, zoomTranslation, Space.Self);
+        moveCamera();
 
         populationChartScript.DisplayPopulations();
 
@@ -47,6 +39,42 @@ public class UserControl : MonoBehaviour
         } else if (Input.GetKeyUp(KeyCode.Tab)) {
             //lineGraphCamera.SetActive(false);
             populationChartCamera.SetActive(false);
+        }
+    }
+
+    // Helper to move the camera
+    void moveCamera()
+    {
+        float horizontalTranslation = Input.GetAxis("Horizontal") * (horizontalSpeed * Mathf.Clamp01((2 + transform.position.y) / 20.0f));
+        float verticalTranslation = Input.GetAxis("Vertical") * (verticalSpeed * Mathf.Clamp01((2 + transform.position.y) / 20.0f));
+        float zoomTranslation = Input.GetAxis("Zoom") * zoomSpeed;
+
+        horizontalTranslation *= Time.deltaTime;
+        verticalTranslation *= Time.deltaTime;
+        zoomTranslation *= Time.deltaTime;
+
+        float horizontalChange = transform.position.x + horizontalTranslation;
+        float verticalChange = transform.position.z + verticalTranslation;
+        float zoomChange = transform.position.z + zoomTranslation;
+
+        if (horizontalChange > 60.0f) {
+            horizontalTranslation = 60.0f - transform.position.x;
+        } else if (horizontalChange < 0.0f) {
+            horizontalTranslation = 0.0f - transform.position.x;
+        }
+
+        if (zoomChange > 20.0f) {
+            zoomTranslation = 20.0f - transform.position.z;
+        } else if (zoomChange < -20.0f) {
+            zoomTranslation = -20.0f - transform.position.z;
+        }
+
+        Debug.Log(transform.position);
+        
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Q)) {
+            transform.Translate(0, 0, zoomTranslation, Space.Self);
+        } else {
+            transform.Translate(horizontalTranslation, 0, verticalTranslation, Space.World);
         }
     }
 }
